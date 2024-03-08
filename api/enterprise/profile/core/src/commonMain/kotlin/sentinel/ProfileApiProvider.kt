551b869a575505ffcb8b90fb44ff92ff12a0552e
George Sechu <admin@mitikaz.com>
2024-03-08 14:16:38 +0300

    Before auto merging main

--- api/enterprise/profile/core/src/commonMain/kotlin/sentinel/ProfileApiProvider.kt
+++ api/enterprise/profile/core/src/commonMain/kotlin/sentinel/ProfileApiProvider.kt
@@ -7,10 +7,12 @@ import koncurrent.later.andThen
 import koncurrent.later.andZip
 import koncurrent.later.zip
 import koncurrent.later.catch
+import kotlinx.JsName
 
 interface ProfileApiProvider {
     val profile: ProfileApi
 
+    @JsName("settingsWithData")
     fun <R> settings(data: R): Later<IdentifierSettings<R>>
     fun settings() = settings(null)
 }
